93b171f3605aff7c3468d1c01f0afa773c89b58a
dani <dani@Linux-PC>
2016-03-03 21:06:06 +0100

    Main file

--- pong/main.c
+++ pong/main.c
@@ -3,65 +3,66 @@
 int main() {
 
 	SDL_Surface* screenGeneral = NULL;
-	SDL_Surface* image = NULL;
+	SDL_Surface* ball = NULL;
 	SDL_Surface* bar = NULL;
 	SDL_Event event;
-	SDL_Rect posicion_image, posicion_barra;
-	posicion_image.x = 150;
-	posicion_image.y = 100;
+	SDL_Rect posicion_ball, posicion_barra;
+	posicion_ball.x = 150;
+	posicion_ball.y = 100;
 	posicion_barra.x = 250;
 	posicion_barra.y = 750;
 
 	int True = 1;
 	int x = 1, y = 1;
 	int movImage = 5, moveBar = 5, moveBarLeftTrue = 0, moveBarRightTrue = 0;
+	int delay = 15;
 
 	SDL_Init (SDL_INIT_EVERYTHING);
 
-	image = SDL_LoadBMP("./icon/icon1.bmp");
+	ball = SDL_LoadBMP("./icon/icon1.bmp");
 	bar = SDL_LoadBMP("./icon/barra.bmp");
 	SDL_WM_SetIcon(SDL_LoadBMP("./icon/icon.bmp"), NULL);
 	screenGeneral = SDL_SetVideoMode(600, 800, 32, SDL_SWSURFACE);
-
-
 	
 	while (True == 1) {
 		if (x == 1 && y == 1) {
-			posicion_image.x = posicion_image.x + movImage;
-			posicion_image.y = posicion_image.y + movImage;
+			posicion_ball.x = posicion_ball.x + movImage;
+			posicion_ball.y = posicion_ball.y + movImage;
 		} else if (x == 0 && y == 1) {
-			posicion_image.x = posicion_image.x - movImage;
-			posicion_image.y = posicion_image.y + movImage;
+			posicion_ball.x = posicion_ball.x - movImage;
+			posicion_ball.y = posicion_ball.y + movImage;
 		} else if (x == 1 && y == 0) {
-			posicion_image.x = posicion_image.x + movImage;
-			posicion_image.y = posicion_image.y - movImage;
+			posicion_ball.x = posicion_ball.x + movImage;
+			posicion_ball.y = posicion_ball.y - movImage;
 		} else {
-			posicion_image.x = posicion_image.x - movImage;
-			posicion_image.y = posicion_image.y - movImage;
+			posicion_ball.x = posicion_ball.x - movImage;
+			posicion_ball.y = posicion_ball.y - movImage;
 		}
-
-		if (posicion_image.x + 48 > 600 || ) {
+		
+		if (posicion_ball.x + ball->w > 600) {
 			x = 0;
 		}
-		if (posicion_image.y + 48 > 800) {
+		if (posicion_ball.y + ball->h > posicion_barra.y && 
+		    posicion_ball.x + ball->w > posicion_barra.x && 
+		    posicion_ball.x < posicion_barra.x + bar->w) {
 			y = 0;
 		}
-		if (posicion_image.x < 0) {
+		if (posicion_ball.x < 0) {
 			x = 1;
 		}
-		if (posicion_image.y < 0) {
+		if (posicion_ball.y < 0) {
 			y = 1;
 		}
 		
 		SDL_FillRect (screenGeneral, 0, SDL_MapRGB (screenGeneral->format, 0, 0, 0));
-		SDL_BlitSurface(image, NULL, screenGeneral, &posicion_image);
+		SDL_BlitSurface(ball, NULL, screenGeneral, &posicion_ball);
 		SDL_BlitSurface(bar, NULL, screenGeneral, &posicion_barra);
 		SDL_Flip(screenGeneral);
-		SDL_Delay(15);
+		SDL_Delay(delay);
 		
 		while(SDL_PollEvent(&event)) {
 			if (event.type == SDL_QUIT) {
-				SDL_FreeSurface(image);
+				SDL_FreeSurface(ball);
 				SDL_Quit();
 				return 0;
 			}
@@ -83,14 +84,18 @@ int main() {
 			}
 			
 		}
-		if (moveBarLeftTrue == 1) {
+		if (moveBarLeftTrue == 1 && posicion_barra.x > 0) {
 			posicion_barra.x = posicion_barra.x - moveBar;
 		}
-		if (moveBarRightTrue == 1) {
+		if (moveBarRightTrue == 1 && posicion_barra.x + bar->w < 600) {
 			posicion_barra.x = posicion_barra.x + moveBar;
 		}
 		
-		
+		if (posicion_ball.y + ball->h > 800) {
+			printf("Game Over\n");
+			SDL_Quit();
+			return 0;
+		}
 	}
 
 	return 0;
